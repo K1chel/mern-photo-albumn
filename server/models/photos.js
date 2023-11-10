a558a476fcdfec5cadaebbe75b54e484672c6044
Your Name <cherevko022@gmail.com>
2023-11-10 12:50:27 -0500

    #3 - add models

---
+++ server/models/photos.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const photoSchema = mongoose.Schema(
+  {
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Photo = mongoose.model("Photo", photoSchema);
+
+export default Photo;
