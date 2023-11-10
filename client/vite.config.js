7d51ed4ace5133f8ddab516c79a4896b092ef8bc
Your Name <cherevko022@gmail.com>
2023-11-10 14:06:39 -0500

    #8 - init client && init shadcn

---
+++ client/vite.config.js
@@ -0,0 +1,27 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
